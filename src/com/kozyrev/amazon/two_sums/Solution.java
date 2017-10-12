65662a8bd2977bab0edddd55d53003e31a168db7
Sergii Kozyrev <kozyrevsergey89@gmail.com>
2017-10-12 23:17:11 +0300

    two sums

---
+++ src/com/kozyrev/amazon/two_sums/Solution.java
@@ -0,0 +1,23 @@
+package com.kozyrev.amazon.two_sums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+
+            if (map.containsKey(target - nums[i])) {
+                res[1] = i;
+                res[0] = map.get(target - nums[i]);
+                return res;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
